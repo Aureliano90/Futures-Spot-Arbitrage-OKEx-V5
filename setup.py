b079076e42d5373ada9ad0084517e33f65ad0d65
Aureliano <81753529+aureliano90@users.noreply.github.com>
2022-01-19 16:59:34 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Futures-Spot-Arbitrage-OKEx-V5',
-    version='0.9',
+    version='0.94',
     packages=find_packages(),
     url='https://github.com/Aureliano90/Futures-Spot-Arbitrage-OKEx-V5',
     license='GNU Affero General Public License v3.0',
